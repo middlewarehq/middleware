e2dad9ca1609d4fc475f17d3173f7d6e78612e6e
Samad Yar Khan <smdyarkhan123@gmail.com>
2024-04-05 15:39:24 +0530

    Add APIs to fetch excluded PRs

--- apiserver/app.py
+++ apiserver/app.py
@@ -5,8 +5,10 @@ load_app_env()
 
 from dora.api.hello import app as core_api
 from dora.api.settings import app as settings_api
+from dora.api.pull_requests import app as pull_requests_api
 
 app = Flask(__name__)
 
 app.register_blueprint(core_api)
 app.register_blueprint(settings_api)
+app.register_blueprint(pull_requests_api)
