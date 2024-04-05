299a8d08cd2c7423d1f32a016d79666d0274982e
Samad Yar Khan <smdyarkhan123@gmail.com>
2024-04-05 12:26:35 +0530

    Add team, user, org settings APIs

--- apiserver/app.py
+++ apiserver/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from env import load_app_env
 
-from dora.api.hello import app as core_api
-
 load_app_env()
+
+from dora.api.hello import app as core_api
+from dora.api.settings import app as settings_api
 
 app = Flask(__name__)
 
 app.register_blueprint(core_api)
+app.register_blueprint(settings_api)
