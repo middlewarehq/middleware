a68f2a75dbb8f266119089793bafd66db0be394c
Samad Yar Khan <smdyarkhan123@gmail.com>
2024-04-09 13:58:26 +0530

    Add repo methods to Code Repo for crud of PRs witth filter

--- apiserver/dora/store/repos/code.py
+++ apiserver/dora/store/repos/code.py
@@ -3,6 +3,7 @@ from typing import Optional, List
 
 from sqlalchemy import or_
 from sqlalchemy.orm import defer
+from dora.utils.time import Interval
 
 from dora.store import rollback_on_exc, session
 from dora.store.models.code import (
@@ -12,6 +13,9 @@ from dora.store.models.code import (
     PullRequestRevertPRMapping,
     PullRequestCommit,
     Bookmark,
+    TeamRepos,
+    PullRequestState,
+    PRFilter,
 )
 
 
@@ -158,3 +162,93 @@ class CodeRepoService:
     ):
         [session.merge(revert_pr_map) for revert_pr_map in revert_pr_mappings]
         session.commit()
+
+    @rollback_on_exc
+    def get_active_team_repos_by_team_id(self, team_id: str) -> List[TeamRepos]:
+        return (
+            session.query(TeamRepos)
+            .filter(TeamRepos.team_id == team_id, TeamRepos.is_active.is_(True))
+            .all()
+        )
+
+    @rollback_on_exc
+    def get_active_team_repos_by_team_ids(self, team_ids: List[str]) -> List[TeamRepos]:
+        return (
+            session.query(TeamRepos)
+            .filter(TeamRepos.team_id.in_(team_ids), TeamRepos.is_active.is_(True))
+            .all()
+        )
+
+    @rollback_on_exc
+    def get_active_org_repos_by_ids(self, repo_ids: List[str]) -> List[OrgRepo]:
+        return (
+            session.query(OrgRepo)
+            .filter(OrgRepo.id.in_(repo_ids), OrgRepo.is_active.is_(True))
+            .all()
+        )
+
+    @rollback_on_exc
+    def get_prs_merged_in_interval(
+        self,
+        repo_ids: List[str],
+        interval: Interval,
+        pr_filter: PRFilter = None,
+        base_branches: List[str] = None,
+    ) -> List[PullRequest]:
+        query = session.query(PullRequest).options(defer(PullRequest.data))
+
+        query = self._filter_prs_by_repo_ids(query, repo_ids)
+        query = self._filter_prs_merged_in_interval(query, interval)
+
+        query = self._filter_prs(query, pr_filter)
+        query = self._filter_base_branch_on_regex(query, base_branches)
+
+        query = query.order_by(PullRequest.state_changed_at.asc())
+
+        return query.all()
+
+    @rollback_on_exc
+    def get_pull_request_by_id(self, pr_id: str) -> PullRequest:
+        return (
+            session.query(PullRequest)
+            .options(defer(PullRequest.data))
+            .filter(PullRequest.id == pr_id)
+            .one_or_none()
+        )
+
+    @rollback_on_exc
+    def get_previous_pull_request(self, pull_request: PullRequest) -> PullRequest:
+        return (
+            session.query(PullRequest)
+            .options(defer(PullRequest.data))
+            .filter(
+                PullRequest.repo_id == pull_request.repo_id,
+                PullRequest.state_changed_at < pull_request.state_changed_at,
+                PullRequest.base_branch == pull_request.base_branch,
+                PullRequest.state == PullRequestState.MERGED,
+            )
+            .order_by(PullRequest.state_changed_at.desc())
+            .first()
+        )
+
+    def _filter_prs_by_repo_ids(self, query, repo_ids: List[str]):
+        return query.filter(PullRequest.repo_id.in_(repo_ids))
+
+    def _filter_prs_merged_in_interval(self, query, interval: Interval):
+        return query.filter(
+            PullRequest.state_changed_at.between(interval.from_time, interval.to_time),
+            PullRequest.state == PullRequestState.MERGED,
+        )
+
+    def _filter_prs(self, query, pr_filter: PRFilter):
+        if pr_filter:
+            query = query.filter(*pr_filter.filter_query)
+        return query
+
+    def _filter_base_branch_on_regex(self, query, base_branches: List[str] = None):
+        if base_branches:
+            conditions = [
+                PullRequest.base_branch.op("~")(term) for term in base_branches
+            ]
+            return query.filter(or_(*conditions))
+        return query
