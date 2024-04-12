75b6a96d716c80d7c5ccace45ba354de1d9f079d
amoghjalan <amoghjalan2005@gmail.com>
2024-04-12 18:01:11 +0530

    Fix GitHub Actions ETL

--- apiserver/dora/service/workflows/sync/etl_handler.py
+++ apiserver/dora/service/workflows/sync/etl_handler.py
@@ -5,11 +5,13 @@ from uuid import uuid4
 from dora.service.code import get_code_integration_service
 from dora.service.workflows.integration import get_workflows_integrations_service
 from dora.service.workflows.sync.etl_provider_handler import WorkflowProviderETLHandler
+from dora.service.workflows.sync.etl_workflows_factory import WorkflowETLFactory
 from dora.store.models.code import (
     OrgRepo,
     RepoWorkflow,
     RepoWorkflowRunsBookmark,
     RepoWorkflowRuns,
+    RepoWorkflowProviders,
 )
 from dora.store.repos.code import CodeRepoService
 from dora.store.repos.workflows import WorkflowRepoService
@@ -22,11 +24,11 @@ class WorkflowETLHandler:
         self,
         code_repo_service: CodeRepoService,
         workflow_repo_service: WorkflowRepoService,
-        etl_service: WorkflowProviderETLHandler,
+        etl_factory: WorkflowETLFactory,
     ):
         self.code_repo_service = code_repo_service
         self.workflow_repo_service = workflow_repo_service
-        self.etl_service = etl_service
+        self.etl_factory = etl_factory
 
     def sync_org_workflows(self, org_id: str):
         active_repo_workflows: List[
@@ -38,7 +40,7 @@ class WorkflowETLHandler:
                 self._sync_repo_workflow(org_repo, repo_workflow)
             except Exception as e:
                 LOG.error(
-                    f"Error syncing workflow for repo {repo_workflow.repo_id}: {str(e)}"
+                    f"Error syncing workflow for repo {repo_workflow.org_repo_id}: {str(e)}"
                 )
                 continue
 
@@ -61,29 +63,37 @@ class WorkflowETLHandler:
         active_repo_workflows: List[
             RepoWorkflow
         ] = self.workflow_repo_service.get_active_repo_workflows_by_repo_ids_and_providers(
-            repo_ids, workflow_providers
+            repo_ids,
+            [RepoWorkflowProviders(provider) for provider in workflow_providers],
         )
         org_repo_workflows: List[Tuple[OrgRepo, RepoWorkflow]] = []
         for repo_workflow in active_repo_workflows:
             org_repo_workflows.append(
-                (repo_id_org_repo_map[str(repo_workflow.repo_id)], repo_workflow)
+                (repo_id_org_repo_map[str(repo_workflow.org_repo_id)], repo_workflow)
             )
         return org_repo_workflows
 
     def _sync_repo_workflow(self, org_repo: OrgRepo, repo_workflow: RepoWorkflow):
+        workflow_provider: RepoWorkflowProviders = repo_workflow.provider
+        etl_service: WorkflowProviderETLHandler = self.etl_factory(
+            workflow_provider.name
+        )
+        if not etl_service.check_pat_validity():
+            LOG.error("Invalid PAT for code provider")
+            return
         try:
             bookmark: RepoWorkflowRunsBookmark = self.__get_repo_workflow_bookmark(
                 repo_workflow
             )
             repo_workflow_runs: List[RepoWorkflowRuns]
-            repo_workflow_runs, bookmark = self.etl_service.get_workflow_runs(
+            repo_workflow_runs, bookmark = etl_service.get_workflow_runs(
                 org_repo, repo_workflow, bookmark
             )
             self.workflow_repo_service.save_repo_workflow_runs(repo_workflow_runs)
             self.workflow_repo_service.update_repo_workflow_runs_bookmark(bookmark)
         except Exception as e:
             LOG.error(
-                f"Error syncing workflow for repo {repo_workflow.repo_id}: {str(e)}"
+                f"Error syncing workflow for repo {repo_workflow.org_repo_id}: {str(e)}"
             )
             return
 
@@ -115,10 +125,10 @@ def sync_org_workflows(org_id: str):
     if not workflow_providers:
         LOG.info(f"No workflow integrations found for org {org_id}")
         return
-    code_repo_service = get_code_integration_service()
+    code_repo_service = CodeRepoService()
     workflow_repo_service = WorkflowRepoService()
-    etl_service = WorkflowProviderETLHandler()
+    etl_factory = WorkflowETLFactory(org_id)
     workflow_etl_handler = WorkflowETLHandler(
-        code_repo_service, workflow_repo_service, etl_service
+        code_repo_service, workflow_repo_service, etl_factory
     )
     workflow_etl_handler.sync_org_workflows(org_id)
