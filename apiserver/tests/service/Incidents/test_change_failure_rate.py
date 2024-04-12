a6d447df8e33045d2b5357368bcbcd443222a2b4
Samad Yar Khan <smdyarkhan123@gmail.com>
2024-04-12 18:13:58 +0530

    Add test for CFR trends

--- apiserver/tests/service/Incidents/test_change_failure_rate.py
+++ apiserver/tests/service/Incidents/test_change_failure_rate.py
@@ -1,7 +1,9 @@
+import pytz
+from datetime import datetime
 from datetime import timedelta
 from tests.factories.models.incidents import get_change_failure_rate_metrics
 from dora.service.incidents.incidents import get_incident_service
-from dora.utils.time import time_now
+from dora.utils.time import Interval, time_now
 
 from tests.factories.models import get_incident, get_deployment
 
@@ -124,34 +126,28 @@ def test_get_change_failure_rate_for_one_incidents_after_two_deployments():
 
 # Multiple incidents and deployments
 def test_get_change_failure_rate_for_multi_incidents_multi_deployments():
-    """
-    Time Line:
-
-    incident_0
-    deployment1
-    deployment_2
-    incident_1
-    deployment_3
-    incident_2
-    deployment_4
-    incident_3
-    deployment_5
-    incident_4
-    incident_5
-    incident_6
-    deployment_6
-
-    """
 
     incident_service = get_incident_service()
 
     incident_0 = get_incident(creation_date=time_now() - timedelta(days=10))
+
+    deployment_1 = get_deployment(conducted_at=time_now() - timedelta(days=7))
+
+    deployment_2 = get_deployment(conducted_at=time_now() - timedelta(days=6))
     incident_1 = get_incident(creation_date=time_now() - timedelta(days=5))
+
+    deployment_3 = get_deployment(conducted_at=time_now() - timedelta(days=4))
     incident_2 = get_incident(creation_date=time_now() - timedelta(days=3))
+
+    deployment_4 = get_deployment(conducted_at=time_now() - timedelta(days=2))
     incident_3 = get_incident(creation_date=time_now() - timedelta(hours=20))
+
+    deployment_5 = get_deployment(conducted_at=time_now() - timedelta(hours=6))
     incident_4 = get_incident(creation_date=time_now() - timedelta(hours=4))
     incident_5 = get_incident(creation_date=time_now() - timedelta(hours=2))
     incident_6 = get_incident(creation_date=time_now() - timedelta(hours=1))
+
+    deployment_6 = get_deployment(conducted_at=time_now() - timedelta(minutes=30))
 
     incidents = [
         incident_0,
@@ -162,13 +158,6 @@ def test_get_change_failure_rate_for_multi_incidents_multi_deployments():
         incident_5,
         incident_6,
     ]
-
-    deployment_1 = get_deployment(conducted_at=time_now() - timedelta(days=7))
-    deployment_2 = get_deployment(conducted_at=time_now() - timedelta(days=6))
-    deployment_3 = get_deployment(conducted_at=time_now() - timedelta(days=4))
-    deployment_4 = get_deployment(conducted_at=time_now() - timedelta(days=2))
-    deployment_5 = get_deployment(conducted_at=time_now() - timedelta(hours=6))
-    deployment_6 = get_deployment(conducted_at=time_now() - timedelta(minutes=30))
 
     deployments = [
         deployment_1,
@@ -198,3 +187,166 @@ def test_get_change_failure_rate_for_multi_incidents_multi_deployments():
         ),
     )
     assert change_failure_rate.change_failure_rate == (4 / 6 * 100)
+
+
+# No Incidents and Deployments
+def test_get_weekly_change_failure_rate_for_no_incidents_no_deployments():
+
+    first_week_2024 = datetime(2024, 1, 1, 0, 0, 0, tzinfo=pytz.UTC)
+    second_week_2024 = datetime(2024, 1, 8, 0, 0, 0, tzinfo=pytz.UTC)
+    third_week_2024 = datetime(2024, 1, 15, 0, 0, 0, tzinfo=pytz.UTC)
+
+    from_time = first_week_2024 + timedelta(days=1)
+    to_time = third_week_2024 + timedelta(days=2)
+
+    incidents = []
+    deployments = []
+
+    incident_service = get_incident_service()
+    weekly_change_failure_rate = incident_service.get_weekly_change_failure_rate(
+        Interval(from_time, to_time),
+        deployments,
+        incidents,
+    )
+    assert weekly_change_failure_rate == {
+        first_week_2024: get_change_failure_rate_metrics([], []),
+        second_week_2024: get_change_failure_rate_metrics([], []),
+        third_week_2024: get_change_failure_rate_metrics([], []),
+    }
+
+
+# No Incidents and Deployments
+def test_get_weekly_change_failure_rate_for_no_incidents_and_some_deployments():
+
+    first_week_2024 = datetime(2024, 1, 1, 0, 0, 0, tzinfo=pytz.UTC)
+    second_week_2024 = datetime(2024, 1, 8, 0, 0, 0, tzinfo=pytz.UTC)
+    third_week_2024 = datetime(2024, 1, 15, 0, 0, 0, tzinfo=pytz.UTC)
+
+    from_time = first_week_2024 + timedelta(days=1)
+    to_time = third_week_2024 + timedelta(days=2)
+
+    deployment_1 = get_deployment(conducted_at=from_time + timedelta(days=2))
+    deployment_2 = get_deployment(conducted_at=second_week_2024 + timedelta(days=2))
+    deployment_3 = get_deployment(conducted_at=to_time - timedelta(hours=2))
+
+    deployments = [deployment_1, deployment_2, deployment_3]
+
+    incidents = []
+
+    incident_service = get_incident_service()
+    weekly_change_failure_rate = incident_service.get_weekly_change_failure_rate(
+        Interval(from_time, to_time),
+        deployments,
+        incidents,
+    )
+
+    assert weekly_change_failure_rate == {
+        first_week_2024: get_change_failure_rate_metrics([], set([deployment_1])),
+        second_week_2024: get_change_failure_rate_metrics([], set([deployment_2])),
+        third_week_2024: get_change_failure_rate_metrics([], set([deployment_3])),
+    }
+
+
+# No Incidents and Deployments
+def test_get_weekly_change_failure_rate_for_incidents_and_no_deployments():
+
+    first_week_2024 = datetime(2024, 1, 1, 0, 0, 0, tzinfo=pytz.UTC)
+    second_week_2024 = datetime(2024, 1, 8, 0, 0, 0, tzinfo=pytz.UTC)
+    third_week_2024 = datetime(2024, 1, 15, 0, 0, 0, tzinfo=pytz.UTC)
+
+    from_time = first_week_2024 + timedelta(days=1)
+    to_time = third_week_2024 + timedelta(days=2)
+
+    incident_1 = get_incident(creation_date=to_time - timedelta(days=14))
+    incident_2 = get_incident(creation_date=to_time - timedelta(days=10))
+    incident_3 = get_incident(creation_date=to_time - timedelta(days=7))
+    incident_4 = get_incident(creation_date=to_time - timedelta(days=3))
+    incident_5 = get_incident(creation_date=to_time - timedelta(hours=2))
+    incident_6 = get_incident(creation_date=to_time - timedelta(hours=1))
+
+    incidents = [incident_1, incident_2, incident_3, incident_4, incident_5, incident_6]
+    deployments = []
+
+    incident_service = get_incident_service()
+    weekly_change_failure_rate = incident_service.get_weekly_change_failure_rate(
+        Interval(from_time, to_time),
+        deployments,
+        incidents,
+    )
+
+    assert weekly_change_failure_rate == {
+        first_week_2024: get_change_failure_rate_metrics([], []),
+        second_week_2024: get_change_failure_rate_metrics([], []),
+        third_week_2024: get_change_failure_rate_metrics([], []),
+    }
+
+
+def test_get_weekly_change_failure_rate_for_incidents_and_deployments():
+
+    first_week_2024 = datetime(2024, 1, 1, 0, 0, 0, tzinfo=pytz.UTC)
+    second_week_2024 = datetime(2024, 1, 8, 0, 0, 0, tzinfo=pytz.UTC)
+    third_week_2024 = datetime(2024, 1, 15, 0, 0, 0, tzinfo=pytz.UTC)
+    fourth_week_2024 = datetime(2024, 1, 22, 0, 0, 0, tzinfo=pytz.UTC)
+
+    from_time = first_week_2024 + timedelta(days=1)
+    to_time = fourth_week_2024 + timedelta(days=2)
+
+    # Week 1
+    incident_0 = get_incident(creation_date=from_time + timedelta(hours=10))
+
+    deployment_1 = get_deployment(conducted_at=from_time + timedelta(hours=12))
+
+    deployment_2 = get_deployment(conducted_at=from_time + timedelta(hours=24))
+    incident_1 = get_incident(creation_date=from_time + timedelta(hours=28))
+    incident_2 = get_incident(creation_date=from_time + timedelta(hours=29))
+
+    # Week 3
+    deployment_3 = get_deployment(conducted_at=fourth_week_2024 - timedelta(days=4))
+    incident_3 = get_incident(creation_date=fourth_week_2024 - timedelta(days=3))
+    incident_4 = get_incident(creation_date=fourth_week_2024 - timedelta(days=3))
+
+    deployment_4 = get_deployment(conducted_at=fourth_week_2024 - timedelta(days=2))
+
+    deployment_5 = get_deployment(conducted_at=fourth_week_2024 - timedelta(hours=6))
+    incident_5 = get_incident(creation_date=fourth_week_2024 - timedelta(hours=3))
+    incident_6 = get_incident(creation_date=fourth_week_2024 - timedelta(hours=2))
+
+    deployment_6 = get_deployment(conducted_at=fourth_week_2024 - timedelta(minutes=30))
+
+    incidents = [
+        incident_0,
+        incident_1,
+        incident_2,
+        incident_3,
+        incident_4,
+        incident_5,
+        incident_6,
+    ]
+
+    deployments = [
+        deployment_1,
+        deployment_2,
+        deployment_3,
+        deployment_4,
+        deployment_5,
+        deployment_6,
+    ]
+
+    incident_service = get_incident_service()
+    weekly_change_failure_rate = incident_service.get_weekly_change_failure_rate(
+        Interval(from_time, to_time),
+        deployments,
+        incidents,
+    )
+
+    assert weekly_change_failure_rate == {
+        first_week_2024: get_change_failure_rate_metrics(
+            set([deployment_2]), set([deployment_1, deployment_2])
+        ),
+        second_week_2024: get_change_failure_rate_metrics([], []),
+        third_week_2024: get_change_failure_rate_metrics(
+            set([deployment_3, deployment_5]),
+            set([deployment_3, deployment_4, deployment_5, deployment_6]),
+        ),
+        fourth_week_2024: get_change_failure_rate_metrics([], []),
+    }
