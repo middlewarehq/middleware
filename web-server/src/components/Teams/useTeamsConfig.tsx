9b59feeb89a0cfd973c8496eae9ff757b1fa2576
e-for-eshaan <eshaanyadav24@gmail.com>
2024-04-15 19:37:17 +0530

    creates a context-based hook for easy factoring

--- web-server/src/components/Teams/useTeamsConfig.tsx
+++ web-server/src/components/Teams/useTeamsConfig.tsx
@@ -1,12 +1,36 @@
-import { useEffect } from 'react';
+import { createContext, useContext, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 
 import { Integration } from '@/constants/integrations';
 import { useAuth } from '@/hooks/useAuth';
 import { fetchTeams } from '@/slices/team';
 import { useSelector } from '@/store';
+import { DB_OrgRepo } from '@/types/api/org_repo';
+import { Team } from '@/types/api/teams';
+import { BaseRepo } from '@/types/resources';
 
-export const useTeamsConfig = () => {
+interface TeamsCRUDContextType {
+  teamName: string;
+  orgRepos: BaseRepo[];
+  teams: Team[];
+  teamReposMaps: Record<string, DB_OrgRepo[]>;
+}
+
+const TeamsCRUDContext = createContext<TeamsCRUDContextType | undefined>(
+  undefined
+);
+
+export const useTeamCRUD = () => {
+  const context = useContext(TeamsCRUDContext);
+  if (!context) {
+    throw new Error(
+      'useTeamSettings must be used within a TeamsSettingsProvider'
+    );
+  }
+  return context;
+};
+
+export const TeamsCRUDProvider: React.FC = ({ children }) => {
   const dispatch = useDispatch();
   const teamReposMaps = useSelector((s) => s.team.teamReposMaps);
   const teams = useSelector((s) => s.team.teams);
@@ -22,5 +46,16 @@ export const useTeamsConfig = () => {
     );
   }, [dispatch, orgId]);
 
-  return { teams, teamReposMaps, orgRepos };
+  const contextValue: TeamsCRUDContextType = {
+    teamName: '',
+    teamReposMaps,
+    teams,
+    orgRepos
+  };
+
+  return (
+    <TeamsCRUDContext.Provider value={contextValue}>
+      {children}
+    </TeamsCRUDContext.Provider>
+  );
 };
