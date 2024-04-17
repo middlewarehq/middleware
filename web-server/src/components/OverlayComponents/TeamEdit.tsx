9a415c80fb011fc515c60d8016363c03043c05fe
e-for-eshaan <eshaanyadav24@gmail.com>
2024-04-17 09:42:45 +0530

    exposes on save and onDiscard methods to the editing overlay

--- web-server/src/components/OverlayComponents/TeamEdit.tsx
+++ web-server/src/components/OverlayComponents/TeamEdit.tsx
@@ -1,17 +1,23 @@
 import { FC } from 'react';
 
+import { CRUDProps } from '@/components/Teams/CreateTeams';
+
 import { FlexBox } from '../FlexBox';
 import { CreateEditTeams } from '../Teams/CreateTeams';
 
-export const TeamEdit: FC<{ teamId: ID; hideCardComponents?: boolean }> = ({
+export const TeamEdit: FC<CRUDProps> = ({
   teamId,
-  hideCardComponents
+  hideCardComponents,
+  onSave,
+  onDiscard
 }) => {
   return (
     <FlexBox>
       <CreateEditTeams
         teamId={teamId}
         hideCardComponents={hideCardComponents}
+        onDiscard={onDiscard}
+        onSave={onSave}
       />
     </FlexBox>
   );
