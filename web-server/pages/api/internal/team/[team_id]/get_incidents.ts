4770ecc1ada4007c62c3e2231f8708b3354fb240
e-for-eshaan <eshaanyadav24@gmail.com>
2024-04-18 10:10:23 +0530

    updates the API to allow sending of PR filters into the params

--- web-server/pages/api/internal/team/[team_id]/get_incidents.ts
+++ web-server/pages/api/internal/team/[team_id]/get_incidents.ts
@@ -3,10 +3,14 @@ import { isNil, reject } from 'ramda';
 import * as yup from 'yup';
 
 import { getTeamPrs } from '@/api/internal/team/[team_id]/insights';
+import { getAllTeamsReposProdBranchesForOrgAsMap } from '@/api/internal/team/[team_id]/repo_branches';
 import { getTeamRevertedPrs } from '@/api/internal/team/[team_id]/revert_prs';
 import { handleRequest } from '@/api-helpers/axios';
 import { Endpoint } from '@/api-helpers/global';
-import { updatePrFilterParams } from '@/api-helpers/team';
+import {
+  updatePrFilterParams,
+  repoFiltersFromTeamProdBranches
+} from '@/api-helpers/team';
 import { mockDeploymentsWithIncidents } from '@/mocks/incidents';
 import {
   DeploymentWithIncidents,
@@ -22,7 +26,8 @@ const getSchema = yup.object().shape({
   from_date: yup.date().required(),
   to_date: yup.date().required(),
   branches: yup.string().optional().nullable(),
-  repo_filters: yup.mixed().optional().nullable()
+  repo_filters: yup.mixed().optional().nullable(),
+  org_id: yup.string().uuid().required()
 });
 
 const endpoint = new Endpoint(pathSchema);
@@ -36,7 +41,8 @@ endpoint.handle.GET(getSchema, async (req, res) => {
     from_date: rawFromDate,
     to_date: rawToDate,
     branches,
-    repo_filters
+    repo_filters,
+    org_id
   } = req.payload;
   const from_date = startOfDay(new Date(rawFromDate));
   const to_date = endOfDay(new Date(rawToDate));
@@ -50,16 +56,39 @@ endpoint.handle.GET(getSchema, async (req, res) => {
     { branches, repo_filters }
   );
 
+  const teamProdBranchesMap =
+    await getAllTeamsReposProdBranchesForOrgAsMap(org_id);
+  const teamRepoFiltersMap =
+    repoFiltersFromTeamProdBranches(teamProdBranchesMap);
+  const pr_filter = await updatePrFilterParams(
+    team_id,
+    {},
+    {
+      branches: branches,
+      repo_filters: !branches ? teamRepoFiltersMap[team_id] : null
+    }
+  ).then(({ pr_filter }) => ({
+    pr_filter
+  }));
+
   const [deploymentsWithIncident, summaryPrs, revertedPrs] = await Promise.all([
-    getTeamIncidentsWithDeployment({ team_id, from_date, to_date }),
+    getTeamIncidentsWithDeployment({ team_id, from_date, to_date, pr_filter }),
     getTeamPrs({
       team_id,
       branches,
       from_date: from_date,
       to_date: to_date,
       repo_filters
-    }).then((r) => r.data),
-    getTeamRevertedPrs({ ...params, team_id })
+    })
+      .then((r) => r.data)
+      .catch(() => {
+        // TODO: remove mocks from here
+        return mockDeploymentsWithIncidents.summary_prs;
+      }),
+    getTeamRevertedPrs({ ...params, team_id }).catch(() => {
+      // TODO: remove mocks from here
+      return mockDeploymentsWithIncidents.revert_prs;
+    })
   ]);
 
   return res.send({
@@ -73,6 +102,7 @@ export const getTeamIncidentsWithDeployment = async (params: {
   team_id: ID;
   from_date: DateString | Date;
   to_date: DateString | Date;
+  pr_filter: any;
 }) => {
   const [from_time, to_time] = getWeekStartAndEndInterval(
     params.from_date,
@@ -84,7 +114,8 @@ export const getTeamIncidentsWithDeployment = async (params: {
     {
       params: reject(isNil, {
         from_time,
-        to_time
+        to_time,
+        pr_filter: params.pr_filter
       })
     }
   );
