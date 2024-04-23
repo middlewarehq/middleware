9c12e976e0c489a0cfcb9c88ee9a33e897a7f4a0
Adnan Hashmi <hashmi.adnan791@gmail.com>
2024-04-23 11:33:40 +0530

    dockerfile updated

---
+++ setup_utils/start.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+echo 'MHQ_EXTRACT_BACKEND_DEPENDENCIES'
+if [ -f /opt/venv.tar.gz ]; then
+    mkdir -p /opt/venv
+    tar xzf /opt/venv.tar.gz -C /opt/venv --strip-components=2
+    rm -rf /opt/venv.tar.gz
+else
+    echo "Tar file /opt/venv.tar.gz does not exist. Skipping extraction."
+fi
+
+echo 'MHQ_EXTRACT_FRONTEND'
+if [ -f /app/web-server.tar.gz ]; then
+    mkdir -p /app/frontend/web-server
+    tar xzf /app/web-server.tar.gz -C /app/frontend/web-server --strip-components=2
+    rm -rf /app/web-server.tar.gz
+else
+    echo "Tar file /app/web-server.tar.gz does not exist. Skipping extraction."
+fi
+
+pg_ctl stop -D /var/lib/postgres/data;
+
+echo 'MHQ_STARTING SUPERVISOR'
+/usr/bin/supervisord -c /etc/supervisord.conf
--- web-server/scripts/server-init.sh
+++ web-server/scripts/server-init.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /bin/sh
 set -e
 
 DIR=$(dirname $0)
