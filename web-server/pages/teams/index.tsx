6e3bf260b9405ecff0d5345a113a6d2889647d65
e-for-eshaan <eshaanyadav24@gmail.com>
2024-05-02 17:17:21 +0530

    removes useRedirectWithSession from the teams-page, adds check on the github-integration and redirects if not-linked, adds loader

--- web-server/pages/teams/index.tsx
+++ web-server/pages/teams/index.tsx
@@ -1,33 +1,41 @@
+import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import ExtendedSidebarLayout from 'src/layouts/ExtendedSidebarLayout';
 
-import { Authenticated } from '@/components/Authenticated';
 import { FlexBox } from '@/components/FlexBox';
-import { CreateEditTeams } from '@/components/Teams/CreateTeams';
+import { CreateEditTeams, Loader } from '@/components/Teams/CreateTeams';
 import { TeamsList } from '@/components/TeamsList';
 import { Integration } from '@/constants/integrations';
-import { useRedirectWithSession } from '@/constants/useRoute';
+import { ROUTES } from '@/constants/routes';
 import { PageWrapper } from '@/content/PullRequests/PageWrapper';
 import { useAuth } from '@/hooks/useAuth';
 import { fetchTeams } from '@/slices/team';
 import { useDispatch, useSelector } from '@/store';
 import { PageLayout } from '@/types/resources';
+import { depFn } from '@/utils/fn';
 
 function Page() {
-  useRedirectWithSession();
   const dispatch = useDispatch();
-  const { orgId } = useAuth();
+  const {
+    orgId,
+    integrations: { github: isGithubIntegrated }
+  } = useAuth();
   const teamsList = useSelector((state) => state.team.teams);
+  const router = useRouter();
 
   useEffect(() => {
     if (!orgId) return;
+    if (!isGithubIntegrated) {
+      depFn(router.replace, ROUTES.INTEGRATIONS.PATH);
+      return;
+    }
     dispatch(
       fetchTeams({
         org_id: orgId,
         provider: Integration.GITHUB
       })
     );
-  }, [dispatch, orgId]);
+  }, [dispatch, isGithubIntegrated, orgId, router.replace]);
 
   return (
     <PageWrapper
@@ -40,17 +48,19 @@ function Page() {
       showEvenIfNoTeamSelected
       hideAllSelectors
     >
-      <FlexBox col gap={4}>
-        {teamsList.length ? <TeamsList /> : <CreateEditTeams />}
-      </FlexBox>
+      {isGithubIntegrated ? (
+        <FlexBox col gap={4}>
+          {teamsList.length ? <TeamsList /> : <CreateEditTeams />}
+        </FlexBox>
+      ) : (
+        <Loader />
+      )}
     </PageWrapper>
   );
 }
 
 Page.getLayout = (page: PageLayout) => (
-  <Authenticated>
-    <ExtendedSidebarLayout>{page}</ExtendedSidebarLayout>
-  </Authenticated>
+  <ExtendedSidebarLayout>{page}</ExtendedSidebarLayout>
 );
 
 export default Page;
