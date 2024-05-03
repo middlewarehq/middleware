22097ec6160d3dd191596bfb514f4c1d59c5cea9
e-for-eshaan <eshaanyadav24@gmail.com>
2024-05-03 14:51:28 +0530

    fixes reloading of team-selector's teams when redirecting to dora-page

--- web-server/src/components/TeamSelector/useTeamSelectorSetup.tsx
+++ web-server/src/components/TeamSelector/useTeamSelectorSetup.tsx
@@ -1,23 +1,17 @@
-import { AxiosResponse } from 'axios';
 import { format } from 'date-fns';
 import pluralize, { plural } from 'pluralize';
 import { prop } from 'ramda';
 import { useState, useEffect, useCallback, useMemo } from 'react';
 import { useDispatch } from 'react-redux';
 
-import { track } from '@/constants/events';
+import { Integration } from '@/constants/integrations';
+import { FetchState } from '@/constants/ui-states';
 import { useAuth } from '@/hooks/useAuth';
-import { useAxios } from '@/hooks/useAxios';
 import { useStateTeamConfig } from '@/hooks/useStateTeamConfig';
 import { appSlice } from '@/slices/app';
-import { teamSlice } from '@/slices/team';
+import { fetchTeams } from '@/slices/team';
 import { useSelector } from '@/store';
-import { Team } from '@/types/api/teams';
-import {
-  ActiveBranchMode,
-  FetchTeamsResponse,
-  TeamSelectorModes
-} from '@/types/resources';
+import { ActiveBranchMode, TeamSelectorModes } from '@/types/resources';
 
 type UseTeamSelectorSetupArgs = { mode: TeamSelectorModes };
 /**
@@ -27,13 +21,18 @@ type UseTeamSelectorSetupArgs = { mode: TeamSelectorModes };
  */
 export const useTeamSelectorSetup = ({ mode }: UseTeamSelectorSetupArgs) => {
   const dispatch = useDispatch();
-  const { dateRange, singleTeam, singleTeamId, setRange, partiallyUnselected } =
+  const { dateRange, singleTeam, setRange, partiallyUnselected } =
     useStateTeamConfig();
   const {
     orgId,
-    integrations: { github: isGithubIntegrations }
+    integrations: { github: isGithubIntegrated }
   } = useAuth();
   const [showAllTeams, setShowAllTeams] = useState(true);
+  const stateTeams = useSelector((state) => state.team.teams);
+  const loadingTeams = useSelector(
+    (state) => state.team.requests?.teams === FetchState.REQUEST
+  );
+
   const activeBranchMode = useSelector((state) => state.app.branchMode);
   const isAllBranchMode = activeBranchMode === ActiveBranchMode.ALL;
   const isSingleMode = mode === 'single' || mode === 'single-only';
@@ -45,7 +44,6 @@ export const useTeamSelectorSetup = ({ mode }: UseTeamSelectorSetupArgs) => {
   const teamReposProdBranchMap = useSelector(
     (state) => state.app.teamsProdBranchMap
   );
-  const isAppSliceUpdated = useSelector((state) => state.app.isUpdated);
 
   const setProdBranchNamesByTeamId = useCallback(
     (teamId: ID) => {
@@ -64,53 +62,6 @@ export const useTeamSelectorSetup = ({ mode }: UseTeamSelectorSetupArgs) => {
     [dispatch, isAllBranchMode, teamReposProdBranchMap]
   );
 
-  const updateUsers = useCallback(
-    (res: AxiosResponse<FetchTeamsResponse>) => {
-      dispatch(teamSlice.actions.setTeams(res.data.teams));
-      dispatch(teamSlice.actions.setTeamReposMaps(res.data.teamReposMap));
-      dispatch(
-        appSlice.actions.setTeamProdBranchMap(res.data.teamReposProdBranchMap)
-      );
-      dispatch(teamSlice.actions.setOrgRepos(res.data.orgRepos));
-      const teams = res.data.teams.filter((t) => t.id);
-
-      const singleT = teams.find((team) => singleTeamId === team.id);
-      const biggestT = biggestTeam(teams);
-      const teamProdBranchNames =
-        res.data.teamReposProdBranchMap?.[singleT?.id]
-          ?.map((r) => r.prod_branches)
-          .filter(Boolean)
-          .join(',') || '';
-
-      if (singleT) {
-        dispatch(appSlice.actions.setSingleTeam([singleT]));
-        if (!isAppSliceUpdated) {
-          dispatch(
-            appSlice.actions.setBranchState({
-              mode: ActiveBranchMode.PROD,
-              names: teamProdBranchNames
-            })
-          );
-        }
-      } else
-        dispatch(appSlice.actions.setSingleTeam(biggestT ? [biggestT] : []));
-
-      track('APP_TEAM_CHANGE_AUTO', { singleT });
-    },
-    [isAppSliceUpdated, dispatch, singleTeamId]
-  );
-
-  const [payload, { fetch: fetchTeams, loading: loadingTeams }] =
-    useAxios<FetchTeamsResponse>(`/resources/orgs/${orgId}/teams`, {
-      manual: true,
-      onSuccess: updateUsers
-    });
-
-  const apiTeams = useMemo(
-    () => payload?.teams || ([] as Team[]),
-    [payload?.teams]
-  );
-
   useEffect(() => {
     if (!orgId) return;
 
@@ -118,8 +69,16 @@ export const useTeamSelectorSetup = ({ mode }: UseTeamSelectorSetupArgs) => {
     const ids = teamIds.split(',').filter(Boolean);
     if (ids.length) params.include_teams = ids;
 
-    if (isGithubIntegrations) fetchTeams({ params });
-  }, [fetchTeams, isGithubIntegrations, orgId, showAllTeams, teamIds]);
+    if (isGithubIntegrated && !stateTeams.length)
+      dispatch(fetchTeams({ org_id: orgId, provider: Integration.GITHUB }));
+  }, [
+    dispatch,
+    isGithubIntegrated,
+    orgId,
+    showAllTeams,
+    stateTeams.length,
+    teamIds
+  ]);
 
   const dateRangeLabel = !partiallyUnselected
     ? `${format(dateRange[0], 'do MMM')} to ${format(dateRange[1], 'do MMM')}`
@@ -138,7 +97,7 @@ export const useTeamSelectorSetup = ({ mode }: UseTeamSelectorSetupArgs) => {
   return useMemo(
     () => ({
       teams,
-      apiTeams,
+      apiTeams: stateTeams,
       usersMap: {},
       dateRangeLabel,
       teamsLabel,
@@ -153,7 +112,6 @@ export const useTeamSelectorSetup = ({ mode }: UseTeamSelectorSetupArgs) => {
       setProdBranchNamesByTeamId
     }),
     [
-      apiTeams,
       dateRange,
       dateRangeLabel,
       hideDateSelector,
@@ -163,23 +121,9 @@ export const useTeamSelectorSetup = ({ mode }: UseTeamSelectorSetupArgs) => {
       setProdBranchNamesByTeamId,
       setRange,
       showAllTeams,
+      stateTeams,
       teams,
       teamsLabel
     ]
   );
-};
-
-const biggestTeam = (argTeams: Team[]) => {
-  if (!argTeams.length) return null;
-
-  let teams = argTeams;
-
-  let biggest = teams[0];
-
-  teams.forEach((team) => {
-    if (team.member_ids.length <= biggest.member_ids.length) return;
-    biggest = team;
-  });
-
-  return biggest;
 };
