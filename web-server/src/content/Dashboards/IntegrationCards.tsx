c9bf39ce49b746fd37cef66f338b0ea57f5be416
shivam-bit <shivama5tronot@gmail.com>
2024-05-07 00:59:48 +0530

    chore: Update GithubIntegrationCard to use isGithubIntegrated instead of integrations.github

--- web-server/src/content/Dashboards/IntegrationCards.tsx
+++ web-server/src/content/Dashboards/IntegrationCards.tsx
@@ -39,7 +39,7 @@ export const GithubIntegrationCard = () => {
 
   return (
     <FlexBox relative>
-      {integrations.github && (
+      {isGithubIntegrated && (
         <FlexBox
           title="Linked"
           sx={{
