cebf8de68a50822825d9c1b58150bb36fb56f138
e-for-eshaan <eshaanyadav24@gmail.com>
2024-05-13 16:35:41 +0530

    fixes empty-state

--- web-server/src/content/DoraMetrics/Incidents.tsx
+++ web-server/src/content/DoraMetrics/Incidents.tsx
@@ -116,7 +116,7 @@ export const AllIncidentsBody = () => {
   const { trendsSeriesMap } = useDoraMetricsGraph();
   const isTrendSeriesAvailable = head(
     trendsSeriesMap?.changeFailureRateTrends || []
-  )?.data?.some((s) => s.y);
+  )?.data?.some((s) => s?.y);
 
   if (isLoading) return <MiniLoader label="Fetching incidents ..." />;
   if (!allDeployments.length || !isTrendSeriesAvailable)
