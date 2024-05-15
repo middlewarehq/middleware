acc66a883116372c6ac454ef86b0134d2ea7a0e1
e-for-eshaan <eshaanyadav24@gmail.com>
2024-05-15 17:43:55 +0530

    declares types for the responses associated with GH's GraphQL API

--- web-server/pages/api/internal/[org_id]/utils.ts
+++ web-server/pages/api/internal/[org_id]/utils.ts
@@ -2,18 +2,37 @@ import { BaseRepo } from '@/types/resources';
 
 const GITHUB_API_URL = 'https://api.github.com/graphql';
 
-interface Repo {
-  id: string;
+type GithubRepo = {
   name: string;
   url: string;
-  isPrivate: boolean;
-  owner: string;
-}
+  defaultBranchRef?: {
+    name: string;
+  };
+  databaseId: string;
+  description?: string;
+  primaryLanguage?: {
+    name: string;
+  };
+  owner: {
+    login: string;
+  };
+};
+
+type RepoReponse = {
+  data: {
+    search: {
+      edges: {
+        node: GithubRepo;
+      }[];
+    };
+  };
+  message?: string;
+};
 
 export const searchGithubRepos = async (
   pat: string,
   searchString: string
-): Promise<Repo[]> => {
+): Promise<BaseRepo[]> => {
   const query = `
           query($queryString: String!) {
               search(type: REPOSITORY, query: $queryString, first: 50) {
@@ -52,18 +71,16 @@ export const searchGithubRepos = async (
     body: JSON.stringify({ query, variables: { queryString } })
   });
 
-  const responseBody = (await response.json()) as any;
+  const responseBody = (await response.json()) as RepoReponse;
 
   if (!response.ok) {
     throw new Error(`GitHub API error: ${responseBody.message}`);
   }
 
-  const repositories = responseBody.data.search.edges.map(
-    (edge: any) => edge.node
-  );
+  const repositories = responseBody.data.search.edges.map((edge) => edge.node);
 
   return repositories.map(
-    (repo: any) =>
+    (repo) =>
       ({
         id: repo.databaseId,
         name: repo.name,
