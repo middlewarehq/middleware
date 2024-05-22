6f39e9794371faae2e476845aea77acedd3b96cc
e-for-eshaan <eshaanyadav24@gmail.com>
2024-05-22 19:25:07 +0530

    implements loading state to show sync-status for the team's repos

--- web-server/pages/dora-metrics/index.tsx
+++ web-server/pages/dora-metrics/index.tsx
@@ -1,17 +1,19 @@
-import { Chip } from '@mui/material';
 import ExtendedSidebarLayout from 'src/layouts/ExtendedSidebarLayout';
 
 import { Authenticated } from '@/components/Authenticated';
 import { FlexBox } from '@/components/FlexBox';
 import Loader from '@/components/Loader';
+import { Loader as MiniLoading } from '@/components/Teams/CreateTeams';
 import { FetchState } from '@/constants/ui-states';
 import { useRedirectWithSession } from '@/constants/useRoute';
-import { DoraMetricsBody } from '@/content/DoraMetrics/DoraMetricsBody';
+import {
+  DoraMetricsBody,
+  useSyncedRepos
+} from '@/content/DoraMetrics/DoraMetricsBody';
 import { PageWrapper } from '@/content/PullRequests/PageWrapper';
 import { useAuth } from '@/hooks/useAuth';
 import { useSelector } from '@/store';
 import { PageLayout } from '@/types/resources';
-
 function Page() {
   useRedirectWithSession();
   const isLoading = useSelector(
@@ -21,6 +23,8 @@ function Page() {
     integrations: { github: isGithubIntegrated }
   } = useAuth();
 
+  const { isSyncing } = useSyncedRepos();
+
   return (
     <PageWrapper
       title={
@@ -28,6 +32,15 @@ function Page() {
           DORA metrics
         </FlexBox>
       }
+      additionalFilters={[
+        <FlexBox fullWidth justifyEnd key="loader">
+          {isSyncing && (
+            <FlexBox>
+              <MiniLoading label="Repo sync in progress" />
+            </FlexBox>
+          )}
+        </FlexBox>
+      ]}
       pageTitle="DORA metrics"
       isLoading={isLoading}
       teamDateSelectorMode="single"
