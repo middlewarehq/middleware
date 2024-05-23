ef9e84066ef9dd98e1d3fff354197b6775edc793
e-for-eshaan <eshaanyadav24@gmail.com>
2024-05-23 07:39:59 +0530

    removes repo-sync loader from header of page

--- web-server/pages/dora-metrics/index.tsx
+++ web-server/pages/dora-metrics/index.tsx
@@ -3,13 +3,9 @@ import ExtendedSidebarLayout from 'src/layouts/ExtendedSidebarLayout';
 import { Authenticated } from '@/components/Authenticated';
 import { FlexBox } from '@/components/FlexBox';
 import Loader from '@/components/Loader';
-import { Loader as MiniLoading } from '@/components/Teams/CreateTeams';
 import { FetchState } from '@/constants/ui-states';
 import { useRedirectWithSession } from '@/constants/useRoute';
-import {
-  DoraMetricsBody,
-  useSyncedRepos
-} from '@/content/DoraMetrics/DoraMetricsBody';
+import { DoraMetricsBody } from '@/content/DoraMetrics/DoraMetricsBody';
 import { PageWrapper } from '@/content/PullRequests/PageWrapper';
 import { useAuth } from '@/hooks/useAuth';
 import { useSelector } from '@/store';
@@ -23,8 +19,6 @@ function Page() {
     integrations: { github: isGithubIntegrated }
   } = useAuth();
 
-  const { isSyncing } = useSyncedRepos();
-
   return (
     <PageWrapper
       title={
@@ -32,15 +26,6 @@ function Page() {
           DORA metrics
         </FlexBox>
       }
-      additionalFilters={[
-        <FlexBox fullWidth justifyEnd key="loader">
-          {isSyncing && (
-            <FlexBox>
-              <MiniLoading label="Repo sync in progress" />
-            </FlexBox>
-          )}
-        </FlexBox>
-      ]}
       pageTitle="DORA metrics"
       isLoading={isLoading}
       teamDateSelectorMode="single"
