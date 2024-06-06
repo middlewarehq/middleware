09fe24ef989d58c8b59c378735ae1c5fef614ccc
Samad Yar Khan <smdyarkhan123@gmail.com>
2024-06-07 02:27:32 +0530

    Update dev.sh to populate BEHIND_COMMITS_COUNT in env

--- dev.sh
+++ dev.sh
@@ -4,16 +4,42 @@ docker-compose down
 
 cat env.example > .env
 
+check_internet_connection() {
+  curl -s https://www.google.com > /dev/null 2>&1
+  if [[ $? -ne 0 ]]; then
+    echo "No internet connection. Cannot fetch latest commits."
+    return 1
+  fi
+  return 0
+}
+
+if ! check_internet_connection; then
+  BEHIND_COMMITS_COUNT=0
+else
+  git fetch origin
+  BEHIND_COMMITS_COUNT=$(git rev-list --count main..origin/main)
+fi
+
 GIT_COMMIT_HASH=$(git rev-parse main)
 GIT_COMMIT_DATE=$(git show -s --format=%cI $MERGE_COMMIT_SHA)
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    sed -i '' "s/^BUILD_DATE=.*/BUILD_DATE=$GIT_COMMIT_DATE/" .env
-    sed -i '' "s/^MERGE_COMMIT_SHA=.*/MERGE_COMMIT_SHA=$GIT_COMMIT_HASH/" .env
-else
-    sed -i "s/^BUILD_DATE=.*/BUILD_DATE=$GIT_COMMIT_DATE/" .env
-    sed -i "s/^MERGE_COMMIT_SHA=.*/MERGE_COMMIT_SHA=$GIT_COMMIT_HASH/" .env
-fi
+update_or_add_env_var() {
+  local var_name=$1
+  local var_value=$2
+  if grep -q "^$var_name=" .env; then
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+      sed -i '' "s/^$var_name=.*/$var_name=$var_value/" .env
+    else
+      sed -i "s/^$var_name=.*/$var_name=$var_value/" .env
+    fi
+  else
+    echo "$var_name=$var_value" >> .env
+  fi
+}
+
+update_or_add_env_var "BUILD_DATE" "$GIT_COMMIT_DATE"
+update_or_add_env_var "MERGE_COMMIT_SHA" "$GIT_COMMIT_HASH"
+update_or_add_env_var "BEHIND_COMMITS_COUNT" "$BEHIND_COMMITS_COUNT"
 
 
 cd ./cli || exit
