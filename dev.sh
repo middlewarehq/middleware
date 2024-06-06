5f91e60052e8c3cf5bd85ba76be369b4255a9256
Samad Yar Khan <smdyarkhan123@gmail.com>
2024-06-06 09:48:35 +0530

    Update dev.sh to populate commit and build date

--- dev.sh
+++ dev.sh
@@ -4,6 +4,18 @@ docker-compose down
 
 cat env.example > .env
 
+GIT_COMMIT_HASH=$(git rev-parse main)
+GIT_COMMIT_DATE=$(git show -s --format=%cI $MERGE_COMMIT_SHA)
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' "s/^BUILD_DATE=.*/BUILD_DATE=$GIT_COMMIT_DATE/" .env
+    sed -i '' "s/^MERGE_COMMIT_SHA=.*/MERGE_COMMIT_SHA=$GIT_COMMIT_HASH/" .env
+else
+    sed -i "s/^BUILD_DATE=.*/BUILD_DATE=$GIT_COMMIT_DATE/" .env
+    sed -i "s/^MERGE_COMMIT_SHA=.*/MERGE_COMMIT_SHA=$GIT_COMMIT_HASH/" .env
+fi
+
+
 cd ./cli || exit
 
 { yarn && yarn build; } > /dev/null 2>&1
