a0561d28ade8256ae47ac38f81db8099955ecee1
Sujai Kumar Gupta <b210031@nitsikkim.ac.in>
2024-08-09 16:13:16 +0530

    add tests for get_resolved_incidents

--- backend/analytics_server/tests/conftest.py
+++ backend/analytics_server/tests/conftest.py
@@ -1,15 +1,20 @@
 import pytest
 from app import create_app
 
+from mhq.store import db
+
 
 @pytest.fixture
 def app():
     """New app instance for each test"""
     db_uri = "sqlite:///:memory:"
     app = create_app({"SQLALCHEMY_DATABASE_URI": db_uri, "TESTING": True})
-
+    db.init_app(app)
     with app.app_context():
         yield app
+        # Drop all the tables after the test
+        db.session.remove()
+        db.drop_all()
 
 
 @pytest.fixture
