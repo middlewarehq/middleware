9a5cfcd3c5884774aba96b65769b0526c1e6efde
RISHIKESHk07 <rishikeshkuppala@gmail.com>
2024-08-15 10:50:09 +0530

    Formatted with black

--- backend/analytics_server/mhq/service/code/pr_analytics.py
+++ backend/analytics_server/mhq/service/code/pr_analytics.py
@@ -2,7 +2,7 @@ from mhq.store.models.code import OrgRepo, PullRequest
 from mhq.store.repos.code import CodeRepoService
 
 
-from typing import List,Optional
+from typing import List, Optional
 from mhq.utils.time import Interval
 
 
@@ -12,9 +12,11 @@ class PullRequestAnalyticsService:
 
     def get_prs_by_ids(self, pr_ids: List[str]) -> List[PullRequest]:
         return self.code_repo_service.get_prs_by_ids(pr_ids)
-    
-    def get_prs_merged_without_review(self,team_id:str,interval:Interval) -> List[PullRequest]:
-        return self.code_repo_service.get_prs_merged_without_review(team_id,interval)
+
+    def get_prs_merged_without_review(
+        self, team_id: str, interval: Interval
+    ) -> List[PullRequest]:
+        return self.code_repo_service.get_prs_merged_without_review(team_id, interval)
 
     def get_team_repos(self, team_id: str) -> List[OrgRepo]:
         return self.code_repo_service.get_team_repos(team_id)
