33a473c6384078cf310fe0f82453953baa7888b5
Kamlesh Patel <kamleshpatel12328@gmail.com>
2024-08-22 15:07:01 +0530

    sync after updating syncDays settings

--- web-server/src/components/Settings/SyncDaysSetting.tsx
+++ web-server/src/components/Settings/SyncDaysSetting.tsx
@@ -2,7 +2,7 @@ import { FlexBox } from '../FlexBox';
 import { Box, Button, Divider, TextField } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
 import { Line } from '../Text';
-import { FC, useEffect, useState } from 'react';
+import { FC, useCallback, useEffect, useState } from 'react';
 import { useAuth } from '@/hooks/useAuth';
 import { useSnackbar } from 'notistack';
 import { useDispatch, useSelector } from '@/store';
@@ -11,64 +11,74 @@ import {
   updateDefaultSyncDaysSettings
 } from '@/slices/org';
 import { FetchState } from '@/constants/ui-states';
+import { useEasyState } from '@/hooks/useEasyState';
+import { depFn } from '@/utils/fn';
+
+const MAXIMUM_SYNC_DAYS = 366;
 
 export const SyncDaysSetting: FC = () => {
   const dispatch = useDispatch();
   const { orgId } = useAuth();
   const { enqueueSnackbar } = useSnackbar();
   const initialSyncDays = useSelector((state) => state.org.defaultSyncDays);
-  const [syncDays, setSyncDays] = useState<number>(initialSyncDays);
+  const syncDays = useEasyState<number>(null);
+  const [error, setError] = useState('');
   const isSaving = useSelector(
     (s) => s.org.requests?.defaultSyncDays === FetchState.REQUEST
   );
-  const [error, setError] = useState('');
-
-  const getSyncDays = async (): Promise<number> => {
-    const res = await dispatch(getDefaultSyncDaysSettings({ orgId }));
-    return res.payload as number;
-  };
-
-  const updateSyncDays = async (days: number): Promise<number> => {
-    const res = await dispatch(
-      updateDefaultSyncDaysSettings({ orgId, defaultSyncDays: days })
-    );
-    return res.payload as number;
-  };
 
   const handleDiscard = () => {
-    setSyncDays(initialSyncDays);
+    syncDays.set(initialSyncDays);
     setError('');
   };
 
-  const handleSave = async () => {
-    try {
-      await updateSyncDays(syncDays);
-      enqueueSnackbar('Default Sync Days Updated Successfully', {
-        variant: 'success',
-        autoHideDuration: 3000
-      });
-    } catch (e) {
+  const handleSave = useCallback(async () => {
+    const res = await dispatch(
+      updateDefaultSyncDaysSettings({
+        orgId,
+        defaultSyncDays: syncDays.value
+      })
+    );
+    if (res.meta.requestStatus === 'rejected') {
       handleDiscard();
-      return enqueueSnackbar('Default Sync Days Update Failed', {
+      return enqueueSnackbar('Default sync days update failed', {
         variant: 'error',
         autoHideDuration: 2000
       });
     }
-  };
+    enqueueSnackbar(
+      'Default sync days updated successfully. The data will be synced shortly.',
+      {
+        variant: 'success',
+        autoHideDuration: 3000
+      }
+    );
+  }, [
+    syncDays.value,
+    initialSyncDays,
+    dispatch,
+    orgId,
+    enqueueSnackbar,
+    handleDiscard
+  ]);
 
   useEffect(() => {
     if (!orgId) return;
-    getSyncDays().then((d) => setSyncDays(d));
-  }, []);
+    dispatch(getDefaultSyncDaysSettings({ orgId }));
+  }, [orgId, dispatch]);
+
+  useEffect(() => {
+    depFn(syncDays.set, initialSyncDays);
+  }, [syncDays.set, initialSyncDays]);
 
   return (
     <FlexBox col gap2>
-      <FlexBox big bold gap1 component={Line} white sx={{ fontSize: '20px' }}>
+      <Line bold white fontSize="20px">
         Organization Settings
-      </FlexBox>
+      </Line>
 
       <Divider />
-      {initialSyncDays && (
+      {syncDays.value !== null && (
         <>
           <FlexBox gap1 alignCenter justifyBetween>
             <FlexBox col flexGrow={1}>
@@ -76,28 +86,30 @@ export const SyncDaysSetting: FC = () => {
                 Sync Days
               </Line>
               <Box>
-                The setting to control how many days of past data should be
-                included.
+                Select how many days of past data to load; more days may
+                increase sync time.
               </Box>
             </FlexBox>
 
             <FlexBox alignCenter>
               <TextField
-                value={syncDays}
+                value={syncDays.value}
                 onChange={(e) => {
                   const days = Number(e.target.value);
-                  if (isNaN(days)) setError('Sync days is Not a number');
-                  else if (days < initialSyncDays)
-                    setError('Less than Current Sync Days not allowed');
-                  else if (days > 366)
-                    setError('Sync days cannot be more than 366');
+                  if (isNaN(days)) return;
+                  if (days < initialSyncDays)
+                    setError('Less than current sync days not allowed');
+                  else if (days > MAXIMUM_SYNC_DAYS)
+                    setError(
+                      `Sync days cannot be more than ${MAXIMUM_SYNC_DAYS}`
+                    );
                   else setError('');
-                  setSyncDays(days);
+                  syncDays.set(days);
                 }}
-                placeholder="default sync days"
+                placeholder="Default sync days"
                 sx={{ minWidth: '240px' }}
                 autoComplete="off"
-                error={!!error}
+                error={Boolean(error)}
                 label={error}
               />
             </FlexBox>
@@ -120,23 +132,23 @@ export const SyncDaysSetting: FC = () => {
               }}
               onClick={handleDiscard}
             >
-              DISCARD
+              Discard
             </Button>
             <LoadingButton
               type="submit"
               variant="outlined"
               color="primary"
               loading={isSaving}
-              disabled={syncDays == initialSyncDays || !!error}
+              disabled={syncDays.value === initialSyncDays || Boolean(error)}
               sx={{
                 '&.Mui-disabled': {
                   borderColor: 'secondary.light'
                 },
                 width: '160px'
               }}
-              onMouseDown={handleSave}
+              onClick={handleSave}
             >
-              SAVE
+              Save
             </LoadingButton>
           </FlexBox>
         </>
