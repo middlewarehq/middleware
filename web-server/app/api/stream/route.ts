412071acc3c285a362d1e20b84df819099d33323
Vipin Chaudhary <VipinChaudhary1809@gmail.com>
2024-09-02 17:43:16 +0000

    refactor

--- web-server/app/api/stream/route.ts
+++ web-server/app/api/stream/route.ts
@@ -6,165 +6,61 @@ import { FSWatcher, createReadStream, watch } from 'fs';
 import { handleRequest, handleSyncServerRequest } from '@/api-helpers/axios';
 import { ServiceNames } from '@/constants/service';
 
-export async function GET(request: NextRequest): Promise<Response> {
-  const responseStream = new TransformStream();
-  const writer = responseStream.writable.getWriter();
-  const encoder = new TextEncoder();
-  let timeoutId: NodeJS.Timeout | null = null;
-  let streamClosed = false;
+type LogFile = {
+  path: string;
+  serviceName: ServiceNames;
+};
 
-  const sendStatuses = async () => {
-    if (streamClosed) return;
+type ServiceStatus = {
+  [key in ServiceNames]: { isUp: boolean };
+};
 
-    try {
-      const statuses = await getStatus();
-      const statusData = { type: 'status-update', statuses };
-
-      if (!streamClosed) {
-        await writer.write(
-          encoder.encode(`data: ${JSON.stringify(statusData)}\n\n`)
-        );
-      }
-    } catch (error) {
-      console.error('Error sending statuses:', error);
-    }
-
-    if (!streamClosed) {
-      timeoutId = setTimeout(sendStatuses, 10000);
-    }
-  };
-
-  const logFiles = [
-    {
-      path: '/var/log/apiserver/apiserver.log',
-      serviceName: ServiceNames.API_SERVER
-    },
-    {
-      path: '/var/log/sync_server/sync_server.log',
-      serviceName: ServiceNames.SYNC_SERVER
-    },
-    { path: '/var/log/redis/redis.log', serviceName: ServiceNames.REDIS },
-    {
-      path: '/var/log/postgres/postgres.log',
-      serviceName: ServiceNames.POSTGRES
-    }
-  ];
-
-  let watchers: FSWatcher[] = [];
-  let lastPositions: { [key: string]: number } = {};
-
-  const sendFileContent = async (filePath: string, serviceName: string) => {
-    if (streamClosed) return;
-
-    return new Promise<void>((resolve, reject) => {
-      const stream = createReadStream(filePath, {
-        start: lastPositions[filePath] || 0,
-        encoding: 'utf8'
-      });
-
-      stream.on('data', (chunk) => {
-        if (streamClosed) return;
-        const data = {
-          type: 'log-update',
-          serviceName,
-          content: chunk
-        };
-
-        writer.write(encoder.encode(`data: ${JSON.stringify(data)}\n\n`));
-        lastPositions[filePath] =
-          (lastPositions[filePath] || 0) + Buffer.byteLength(chunk);
-      });
-
-      stream.on('end', () => resolve());
-      stream.on('error', (error) => reject(error));
-    });
-  };
-
-  const startWatchers = () => {
-    logFiles.forEach(async ({ path, serviceName }) => {
-      await sendFileContent(path, serviceName);
-
-      const watcher = watch(path, async (eventType) => {
-        if (eventType === 'change') {
-          await sendFileContent(path, serviceName);
-        }
-      });
-
-      watchers.push(watcher);
-    });
-  };
-
-  const cleanupWatchers = () => {
-    watchers.forEach((watcher) => watcher.close());
-    watchers = [];
-  };
-
-  sendStatuses();
-  startWatchers();
-
-  const closeStream = () => {
-    if (!streamClosed) {
-      streamClosed = true;
-      writer
-        .close()
-        .catch((error) => console.error('Error closing writer:', error));
-      if (timeoutId) {
-        clearTimeout(timeoutId);
-      }
-    }
-    cleanupWatchers();
-  };
-
-  request.signal.onabort = closeStream;
-
-  return new Response(responseStream.readable, {
-    headers: {
-      'Content-Type': 'text/event-stream',
-      Connection: 'keep-alive',
-      'Cache-Control': 'no-cache, no-transform'
-    }
-  });
-}
+const UPDATE_INTERVAL = 10000;
+const LOG_FILES: LogFile[] = [
+  {
+    path: '/var/log/apiserver/apiserver.log',
+    serviceName: ServiceNames.API_SERVER
+  },
+  {
+    path: '/var/log/sync_server/sync_server.log',
+    serviceName: ServiceNames.SYNC_SERVER
+  },
+  { path: '/var/log/redis/redis.log', serviceName: ServiceNames.REDIS },
+  { path: '/var/log/postgres/postgres.log', serviceName: ServiceNames.POSTGRES }
+];
 
 const execPromise = (command: string): Promise<string> => {
   return new Promise((resolve, reject) => {
-    exec(command, (error, stdout, _stderr) => {
-      if (error) {
-        return reject(error);
-      }
+    exec(command, (error, stdout) => {
+      if (error) return reject(error);
       resolve(stdout);
     });
   });
 };
 
-const checkServiceStatus = async (serviceName: string): Promise<boolean> => {
+const checkServiceStatus = async (
+  serviceName: ServiceNames
+): Promise<boolean> => {
   try {
     switch (serviceName) {
-      case ServiceNames.API_SERVER: {
-        const response = await handleRequest('');
-        return response.message.includes('hello world');
-      }
-
-      case ServiceNames.REDIS: {
-        const REDIS_PORT = process.env.REDIS_PORT;
-        const response = await execPromise(`redis-cli -p ${REDIS_PORT} ping`);
-        return response.trim().includes('PONG');
-      }
-
-      case ServiceNames.POSTGRES: {
-        const POSTGRES_PORT = process.env.DB_PORT;
-        const POSTGRES_HOST = process.env.DB_HOST;
-        const response = await execPromise(
-          `pg_isready -h ${POSTGRES_HOST} -p ${POSTGRES_PORT}`
+      case ServiceNames.API_SERVER:
+        return (await handleRequest('')).message.includes('hello world');
+      case ServiceNames.REDIS:
+        return (
+          await execPromise(`redis-cli -p ${process.env.REDIS_PORT} ping`)
+        )
+          .trim()
+          .includes('PONG');
+      case ServiceNames.POSTGRES:
+        return (
+          await execPromise(
+            `pg_isready -h ${process.env.DB_HOST} -p ${process.env.DB_PORT}`
+          )
+        ).includes('accepting connections');
+      case ServiceNames.SYNC_SERVER:
+        return (await handleSyncServerRequest('')).message.includes(
+          'hello world'
         );
-        return response.includes('accepting connections');
-      }
-
-      case ServiceNames.SYNC_SERVER: {
-        const response = await handleSyncServerRequest('');
-        return response.message.includes('hello world');
-      }
-
       default:
         console.warn(`Service ${serviceName} not recognized.`);
         return false;
@@ -175,23 +71,103 @@ const checkServiceStatus = async (serviceName: string): Promise<boolean> => {
   }
 };
 
-const getStatus = async (): Promise<{
-  [key in ServiceNames]: { isUp: boolean };
-}> => {
+const getStatus = async (): Promise<ServiceStatus> => {
   const services = Object.values(ServiceNames);
-  const statuses: { [key in ServiceNames]: { isUp: boolean } } = {
-    [ServiceNames.API_SERVER]: { isUp: false },
-    [ServiceNames.REDIS]: { isUp: false },
-    [ServiceNames.POSTGRES]: { isUp: false },
-    [ServiceNames.SYNC_SERVER]: { isUp: false }
-  };
+  const statuses = {} as ServiceStatus;
 
   await Promise.all(
     services.map(async (service) => {
-      const isUp = await checkServiceStatus(service);
-      statuses[service] = { isUp };
+      statuses[service] = { isUp: await checkServiceStatus(service) };
     })
   );
 
   return statuses;
 };
+
+export async function GET(request: NextRequest): Promise<Response> {
+  const { writable, readable } = new TransformStream();
+  const writer = writable.getWriter();
+  const encoder = new TextEncoder();
+
+  let timeoutId: NodeJS.Timeout | null = null;
+  let streamClosed = false;
+  let watchers: FSWatcher[] = [];
+  let lastPositions: { [key: string]: number } = {};
+
+  const sendEvent = async (eventType: string, data: any) => {
+    if (streamClosed) return;
+    await writer.write(
+      encoder.encode(
+        `data: ${JSON.stringify({ type: eventType, ...data })}\n\n`
+      )
+    );
+  };
+
+  const sendStatuses = async () => {
+    if (streamClosed) return;
+    try {
+      const statuses = await getStatus();
+      await sendEvent('status-update', { statuses });
+    } catch (error) {
+      console.error('Error sending statuses:', error);
+    }
+    if (!streamClosed) {
+      timeoutId = setTimeout(sendStatuses, UPDATE_INTERVAL);
+    }
+  };
+
+  const sendFileContent = async ({ path, serviceName }: LogFile) => {
+    if (streamClosed) return;
+    return new Promise<void>((resolve, reject) => {
+      const stream = createReadStream(path, {
+        start: lastPositions[path] || 0,
+        encoding: 'utf8'
+      });
+      stream.on('data', async (chunk) => {
+        if (streamClosed) return;
+        await sendEvent('log-update', { serviceName, content: chunk });
+        lastPositions[path] =
+          (lastPositions[path] || 0) + Buffer.byteLength(chunk);
+      });
+      stream.on('end', resolve);
+      stream.on('error', reject);
+    });
+  };
+
+  const startWatchers = () => {
+    LOG_FILES.forEach(async (logFile) => {
+      await sendFileContent(logFile);
+      const watcher = watch(logFile.path, async (eventType) => {
+        if (eventType === 'change') {
+          await sendFileContent(logFile);
+        }
+      });
+      watchers.push(watcher);
+    });
+  };
+
+  const cleanup = () => {
+    if (!streamClosed) {
+      streamClosed = true;
+      writer
+        .close()
+        .catch((error) => console.error('Error closing writer:', error));
+      if (timeoutId) clearTimeout(timeoutId);
+    }
+    watchers.forEach((watcher) => watcher.close());
+    watchers = [];
+  };
+
+  sendStatuses();
+  startWatchers();
+
+  request.signal.onabort = cleanup;
+
+  return new Response(readable, {
+    headers: {
+      'Content-Type': 'text/event-stream',
+      Connection: 'keep-alive',
+      'Cache-Control': 'no-cache, no-transform'
+    }
+  });
+}
