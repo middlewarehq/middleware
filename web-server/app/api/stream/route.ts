ea77c61f44031f46ef60727c562da4fd625d9028
Vipin Chaudhary <VipinChaudhary1809@gmail.com>
2024-09-02 05:58:42 +0000

    stream route implemenation

--- web-server/app/api/stream/route.ts
+++ web-server/app/api/stream/route.ts
@@ -1,38 +1,56 @@
-export function GET(req: Request) {
+
+import { NextRequest } from 'next/server';
+export const runtime = 'nodejs';
+export const dynamic = 'force-dynamic';
+
+export async function GET(request: NextRequest) {
+  let responseStream = new TransformStream();
+  const writer = responseStream.writable.getWriter();
   const encoder = new TextEncoder();
-  let count = 1;
-  return new Response(
-    new ReadableStream({
-      start(controller) {
-        var interval = setInterval(() => {
-          console.log('doing something  new1', Date.now());
-          controller.enqueue(encoder.encode(String(Date.now() + '\n' + count)));
-          count++;
+  let count = 0;
+  let timeoutId: NodeJS.Timeout | null = null;
+  let streamClosed = false;
 
-          if (count > 10) {
-            clearInterval(interval);
-            controller.close();
-          }
-        }, 1000);
-
-        req.signal.onabort = () => {
-          console.log('request closed');
-          clearInterval(interval);
-          controller.close();
-        };
-        req.signal.addEventListener('abort', async () => {
-          console.log('abort');
-          clearInterval(interval);
-          controller.close();
-          // req.close();
-          // await writer.close();
-        });
-      }
-    }),
-    {
-      headers: {
-        'content-type': 'text/plain'
+  const closeStream = () => {
+    if (!streamClosed) {
+      streamClosed = true;
+      writer.close().catch((error) => {
+        console.error('Error closing writer:', error);
+      });
+      if (timeoutId !== null) {
+        clearTimeout(timeoutId);
       }
     }
-  );
+  };
+
+  function repeat() {
+    if (streamClosed) return;
+
+    console.log(count);
+    count += 1;
+
+    if (count > 10) {
+      console.log('Complete timeout');
+      closeStream();
+    } else {
+      timeoutId = setTimeout(repeat, 3000); // Recursively call repeat after 3000ms
+    }
+  }
+
+  // Start the first iteration
+  timeoutId = setTimeout(repeat, 3000);
+
+  // Close the stream if the client disconnects and stop the timeout
+  request.signal.onabort = () => {
+    console.log('Client disconnected. Closing writer.');
+    closeStream();
+  };
+
+  return new Response(responseStream.readable, {
+    headers: {
+      'Content-Type': 'text/event-stream',
+      Connection: 'keep-alive',
+      'Cache-Control': 'no-cache, no-transform'
+    }
+  });
 }
