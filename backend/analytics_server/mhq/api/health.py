69e3433f7a681c32c5fbd855f8187976952874cf
VipinDevelops <VipinChaudhary1809@gmail.com>
2024-09-06 22:09:04 +0530

    fix: lint

--- backend/analytics_server/mhq/api/health.py
+++ backend/analytics_server/mhq/api/health.py
@@ -2,8 +2,6 @@ from flask import Blueprint
 
 app = Blueprint("health", __name__)
 
-
 @app.route("/health", methods=["GET"])
 def health_check():
-    
     return {"status": "healthy"}
--- backend/analytics_server/mhq/api/hello.py
+++ backend/analytics_server/mhq/api/hello.py
@@ -2,8 +2,6 @@ from flask import Blueprint
 
 app = Blueprint("hello", __name__)
 
-
 @app.route("/", methods=["GET"])
 def hello_world():
-
     return {"message": "hello world"}
