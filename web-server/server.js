8d8b0e1fae2deb63af51f0a4e800506b9d43ce61
not-meet <mjmeet.279@gmail.com>
2024-10-23 15:59:35 +0530

    changes the web-server from http/1 to http/2

---
+++ web-server/server.js
@@ -0,0 +1,29 @@
+import next from 'next';
+
+import fs from 'fs';
+import { createServer } from 'https';
+import { parse } from 'url';
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const httpsOptions = {
+  key: fs.readFileSync('./certificates/localhost.key'),
+  cert: fs.readFileSync('./certificates/localhost.crt')
+};
+
+app.prepare().then(() => {
+  createServer(httpsOptions, (req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(3333, (err) => {
+    if (err) throw err;
+    console.log('> Server started on https://localhost:3333');
+  });
+});
+
+
+
+
+
